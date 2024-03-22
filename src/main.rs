a413803e984ef33e90c2d763f0ea08588d4e9d5e
Rudolf Hersén <rudolf.hersen@schibsted.com>
2024-03-22 09:05:05 +0100

    move fetch function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
-use chrono;
-use reqwest::header::{HeaderMap, CONTENT_TYPE};
-use reqwest::Error;
 use rocket_dyn_templates::context;
 use rocket_dyn_templates::Template;
-use std::ops::{Add, Sub};
-use train_announcement::TrainAnnouncement;
-use types::Root;
 use types::TrainInfo;
 
 mod train_announcement;
@@ -19,11 +13,11 @@ async fn index() -> Template {
     Template::render("index", {})
 }
 
-const LOCATION_SIGNATURE: &'static str = "Cst";
+const LOCATION_SIGNATURE: &'static str = "Tul";
 
 #[get("/text")]
 async fn text() -> String {
-    match post_xml_data(LOCATION_SIGNATURE).await {
+    match train_announcement::fetch(LOCATION_SIGNATURE).await {
         Ok(announcements) => announcements
             .iter()
             .map(|it| {
@@ -44,7 +38,7 @@ async fn text() -> String {
 
 #[get("/trains")]
 async fn trains() -> Template {
-    match post_xml_data(LOCATION_SIGNATURE).await {
+    match train_announcement::fetch(LOCATION_SIGNATURE).await {
         Ok(announcements) => {
             let mut trains: Vec<TrainInfo> =
                 announcements.iter().map(|it| it.transform()).collect();
@@ -65,53 +59,4 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, text, trains])
         .attach(Template::fairing())
-}
-
-async fn post_xml_data(location_signature: &str) -> Result<Vec<TrainAnnouncement>, Error> {
-    let mut headers = HeaderMap::new();
-    headers.insert(CONTENT_TYPE, "application/xml".parse().unwrap());
-
-    let now = chrono::Utc::now();
-    let since = now.sub(chrono::Duration::hours(1));
-    let until = now.add(chrono::Duration::hours(1));
-
-    let xml_data = format!(
-        r#"
-    <REQUEST>
-        <LOGIN authenticationkey='{}' />
-        <QUERY objecttype='TrainAnnouncement' schemaversion='1.6'>
-            <FILTER>
-                <AND>
-                    <EQ name='LocationSignature' value='{}' />
-                    <GT name='AdvertisedTimeAtLocation' value='{}' />
-                    <LT name='AdvertisedTimeAtLocation' value='{}' />
-                </AND>
-            </FILTER>
-            <INCLUDE>ActivityType</INCLUDE>
-            <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
-            <INCLUDE>AdvertisedTrainIdent</INCLUDE>
-            <INCLUDE>TimeAtLocationWithSeconds</INCLUDE>
-            <INCLUDE>ToLocation</INCLUDE>
-        </QUERY>
-    </REQUEST>
-"#,
-        std::env::var("TRAFIKVERKET_API_KEY").unwrap_or_default(),
-        location_signature,
-        since.to_rfc3339(),
-        until.to_rfc3339()
-    );
-    let res = reqwest::Client::new()
-        .post("https://api.trafikinfo.trafikverket.se/v2/data.json")
-        .headers(headers)
-        .body(xml_data)
-        .send()
-        .await?;
-
-    println!("Status: {}", res.status());
-
-    let data: Root = res.json().await?;
-
-    let mut vec = data.RESPONSE.RESULT[0].TrainAnnouncement.clone();
-    vec.sort_by(|a, b| a.train_ident().cmp(&b.train_ident()));
-    Ok(vec)
 }
--- src/train_announcement.rs
+++ src/train_announcement.rs
@@ -1,7 +1,10 @@
 #![allow(non_snake_case)]
 
-use crate::types::{Location, TrainInfo};
+use crate::types::{Location, Root, TrainInfo};
+use reqwest::header::{HeaderMap, CONTENT_TYPE};
+use reqwest::Error;
 use serde::{Deserialize, Serialize};
+use std::ops::{Add, Sub};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TrainAnnouncement {
@@ -49,4 +52,53 @@ impl TrainAnnouncement {
             time_at_location: self.time_at_location(),
         }
     }
+}
+
+pub(crate) async fn fetch(location_signature: &str) -> Result<Vec<TrainAnnouncement>, Error> {
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, "application/xml".parse().unwrap());
+
+    let now = chrono::Utc::now();
+    let since = now.sub(chrono::Duration::hours(1));
+    let until = now.add(chrono::Duration::hours(1));
+
+    let xml_data = format!(
+        r#"
+    <REQUEST>
+        <LOGIN authenticationkey='{}' />
+        <QUERY objecttype='TrainAnnouncement' schemaversion='1.6'>
+            <FILTER>
+                <AND>
+                    <EQ name='LocationSignature' value='{}' />
+                    <GT name='AdvertisedTimeAtLocation' value='{}' />
+                    <LT name='AdvertisedTimeAtLocation' value='{}' />
+                </AND>
+            </FILTER>
+            <INCLUDE>ActivityType</INCLUDE>
+            <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
+            <INCLUDE>AdvertisedTrainIdent</INCLUDE>
+            <INCLUDE>TimeAtLocationWithSeconds</INCLUDE>
+            <INCLUDE>ToLocation</INCLUDE>
+        </QUERY>
+    </REQUEST>
+"#,
+        std::env::var("TRAFIKVERKET_API_KEY").unwrap_or_default(),
+        location_signature,
+        since.to_rfc3339(),
+        until.to_rfc3339()
+    );
+    let res = reqwest::Client::new()
+        .post("https://api.trafikinfo.trafikverket.se/v2/data.json")
+        .headers(headers)
+        .body(xml_data)
+        .send()
+        .await?;
+
+    println!("Status: {}", res.status());
+
+    let data: Root = res.json().await?;
+
+    let mut vec = data.RESPONSE.RESULT[0].TrainAnnouncement.clone();
+    vec.sort_by(|a, b| a.train_ident().cmp(&b.train_ident()));
+    Ok(vec)
 }
